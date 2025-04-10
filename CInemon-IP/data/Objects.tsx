02f1ccc3a2d4aa017166819a90dff54d5b3ea48b
FelipeBerardo <felipebalv@gmail.com>
2025-04-10 16:32:39 -0300

    Add files via upload

---
+++ CInemon-IP/data/Objects.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="arroz" tilewidth="768" tileheight="768" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../graphics/objects/tree_1.png" width="256" height="256"/>
+ </tile>
+ <tile id="1">
+  <image source="../graphics/objects/tree_2.png" width="256" height="256"/>
+ </tile>
+ <tile id="2">
+  <image source="../graphics/objects/tree_3.png" width="256" height="256"/>
+ </tile>
+ <tile id="3">
+  <image source="../graphics/objects/castle.png" width="768" height="768"/>
+ </tile>
+ <tile id="4">
+  <image source="../graphics/objects/mushroom_1.png" width="64" height="64"/>
+ </tile>
+ <tile id="5">
+  <image source="../graphics/objects/mushroom_2.png" width="64" height="64"/>
+ </tile>
+ <tile id="6">
+  <image source="../graphics/objects/mushroom_3.png" width="64" height="64"/>
+ </tile>
+ <tile id="7">
+  <image source="../graphics/objects/tower.png" width="384" height="384"/>
+ </tile>
+</tileset>
