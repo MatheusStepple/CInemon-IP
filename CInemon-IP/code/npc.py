8267aff5c1359878f80338ae9b6c521bed2a870b
MatheusHenriqueCIN <159284886+MatheusHenriqueCIN@users.noreply.github.com>
2025-04-09 22:47:35 -0300

    codigo bugado com mapa para erro ajeitar

--- CInemon-IP/code/npc.py
+++ CInemon-IP/code/npc.py
@@ -2,18 +2,16 @@ import pygame
 import os
 
 class NPC:
-    def __init__(self, x, y, nome):
+    def init(self, x, y, nome, sprite_file="spr_fernanda_madeiral.png"):
         self.x = x
         self.y = y
         self.nome = nome
         self.width = 40
         self.height = 60
-        self.rect = pygame.Rect(self.x + 8, self.y + 18, 24, 36)  # Hitbox ajustada como o jogador
-        
-        # Carrega o sprite
+        self.rect = pygame.Rect(self.x + 8, self.y + 18, 24, 36)  # Hitbox ajustada
+
         try:
-            sprite_path = os.path.join("Desktop", "CInemon-IP", "sprites", "spr_fernanda_madeiral.png")  
-            
+            sprite_path = os.path.join("Desktop", "CInemon-IP", "sprites", sprite_file)
             self.sprite = pygame.image.load(sprite_path).convert_alpha()
             self.sprite = pygame.transform.scale(self.sprite, (self.width, self.height))
         except Exception as e:
@@ -26,7 +24,7 @@ class NPC:
         pos_y = (self.y - camera.y) * zoom
         scaled_width = int(self.width * zoom)
         scaled_height = int(self.height * zoom)
-        
+
         if self.sprite:
             sprite_scaled = pygame.transform.scale(self.sprite, (scaled_width, scaled_height))
             tela.blit(sprite_scaled, (pos_x, pos_y))
